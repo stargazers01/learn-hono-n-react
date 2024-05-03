1dcf2acfb580d72327b1dd9872f39e408064e8a0
riiido <shiroex7@gmail.com>
2024-05-04 00:18:05 +0700

    feat: change ts to js

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+// import { Fetch } from "hono/client";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// app.get('/api/news/technology', async (c) => {
+//   const techNewsResponse = await fetch('https://www.technews.com/api/news');
+//   const techNewsData = await techNewsResponse.json();
 
-export default app
+//   const technologyNews = await techNewsData.filter((article:any) => article.category === 'technology');
+
+//   return c.json(techNewsData)
+// });
+
+app.get('/api/news/latest', async (c) => {
+  const newsApiResponse = await fetch('https://newsapi.org/v2/top-headlines/sources?country=id&apikey=7aa522a2df2f4d67acd9c6bc655fd297');
+  const newsApiData = await newsApiResponse.json();
+
+  console.log(newsApiData);
+  
+  return c.json(newsApiData);
+});
+
+export default app;
